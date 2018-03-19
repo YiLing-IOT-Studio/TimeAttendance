56d839ac9c6b4b62855f33848b948b8d4198dc13
FantasticPan <FantasticPan@user.com>
2018-03-19 13:42:16 +0800

    加入了MyBatis

--- src/main/java/com/attendance/ProjectApplication.java
+++ src/main/java/com/attendance/ProjectApplication.java
@@ -1,9 +1,11 @@
 package com.attendance;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.attendance.mapper")
 public class ProjectApplication {
 
 	public static void main(String[] args) {
